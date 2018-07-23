38adb2864da5199a17673ca3d44b0fd3e6b55417
David Stensland <me@terite.com>
2018-07-22 23:21:53 -0700

    remove unused pairsByKeys function

--- control.lua
+++ control.lua
@@ -7,20 +7,6 @@ local function round2(num, numDecimalPlaces)
   return tonumber(string.format("%." .. (numDecimalPlaces or 0) .. "f", num))
 end
 
-local function pairsByKeys (t, f)
-  local a = {}
-  for n in pairs(t) do table.insert(a, n) end
-  table.sort(a, f)
-  local i = 0
-  local iter = function ()
-    i = i + 1
-    if a[i] == nil then return nil
-    else return a[i], t[a[i]]
-    end
-  end
-  return iter
-end
-
 local function get_allowed(tbl)
     if tbl == nil then
         return nil
